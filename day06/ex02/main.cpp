18f3ff907a2ac3ad608baecdca810924dd8fe36e
LT <jiloh55@gmail.com>
2021-08-13 21:42:43 +0200

    erased comm ex02 d06

--- day06/ex02/main.cpp
+++ day06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 18:13:49 by ltouret           #+#    #+#             */
-/*   Updated: 2021/08/13 21:40:56 by ltouret          ###   ########.fr       */
+/*   Updated: 2021/08/13 21:41:48 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ Base		*generate(void)
 {
 	srand(time(NULL));
 	int rdm = rand() % 3;
+
 	if (rdm == 0)
 		return static_cast<Base *>(new A());
 	else if (rdm == 1)
@@ -26,9 +27,9 @@ Base		*generate(void)
 
 void		identify(Base * p)
 {
-	A * a = dynamic_cast<A *>(p);
-	B * b = dynamic_cast<B *>(p);
-	C * c = dynamic_cast<C *>(p);
+	A *a = dynamic_cast<A *>(p);
+	B *b = dynamic_cast<B *>(p);
+	C *c = dynamic_cast<C *>(p);
 
 	if (a != NULL)
 		std::cout << "A" << std::endl;
@@ -46,21 +47,18 @@ void		identify(Base & p)
 {
 	try
 	{
-//		A & a = dynamic_cast<A &>(p);
 		(void)dynamic_cast<A &>(p);
 		std::cout << "A" << std::endl;
 	}
 	catch (std::exception &e){}
 	try
 	{
-		//B & b = dynamic_cast<B &>(p);
 		(void)dynamic_cast<B &>(p);
 		std::cout << "B" << std::endl;
 	}
 	catch (std::exception &e){}
 	try
 	{
-		//C & c = dynamic_cast<C &>(p);
 		(void)dynamic_cast<C &>(p);
 		std::cout << "C" << std::endl;
 	}
