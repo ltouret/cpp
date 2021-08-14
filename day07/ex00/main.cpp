77308836e4c076300c01f4e45733ce289ba133f3
LT <jiloh55@gmail.com>
2021-08-14 21:01:57 +0200

    done ex00 d07

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 18:13:49 by ltouret           #+#    #+#             */
-/*   Updated: 2021/08/13 23:50:24 by ltouret          ###   ########.fr       */
+/*   Updated: 2021/08/14 21:01:37 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 int		main(void)
 {
+
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
 	return (0);
 }
--- day07/ex00/whatever.hpp
+++ day07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 22:26:07 by ltouret           #+#    #+#             */
-/*   Updated: 2021/08/13 23:50:39 by ltouret          ###   ########.fr       */
+/*   Updated: 2021/08/14 21:01:24 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 # define WHATEVER_HPP
 
 # include <iostream>
-# include <cstdlib>
-# include <ctype.h>
 
+template<typename T>
+void swap(T &x, T &y)
+{
+	T tmp = x;
+	x = y;
+	y = tmp;
+	return ;
+}
+
+template<typename T>
+T min(T const &x, T const &y)
+{
+	return (x >= y) ? y : x;
+}
+
+template<typename T>
+T max(T const &x, T const &y)
+{
+	return (x <= y) ? y : x;
+}
 
 #endif
