e75c89bef1d6a331aac4d4fceef766ed03dc3ded
LT <jiloh55@gmail.com>
2021-08-15 20:00:55 +0200

    d07 done

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/20 22:26:07 by ltouret           #+#    #+#             */
-/*   Updated: 2021/08/15 11:12:07 by ltouret          ###   ########.fr       */
+/*   Updated: 2021/08/15 19:56:57 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,62 @@
 # include <iostream>
 
 template <typename T>
-class Array {
+class Array
+{
 private:
-    T		*Data;
-    size_t	size;
+	unsigned int		_size;
+	T			*_array;
+
 public:
-    Array(void);
-    Array(unsigned int s);
-    //void print();
+	Array(void): _size(0), _array(new T[0]) {}
+	Array(unsigned int size): _size(size), _array(new T[size]) {}
+
+	Array(Array const &to_cpy)
+	{
+		*this = to_cpy;
+		return ;
+	}
+
+	~Array(void)
+	{
+		if (this->_array)
+			delete[] _array;
+		return ;
+	}
+
+	Array			&operator=(Array const &to_cpy)
+	{
+		if (this != &to_cpy)
+		{
+			if (this->_array)
+				delete[] _array;
+			this->_size = to_cpy._size;
+			this->_array = new T[this->_size];
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->_array[i] = to_cpy._array[i];
+		}
+		return (*this);
+	}
+
+	T			&operator[](unsigned int i)
+	{
+		if (i >= this->_size)
+			throw Array::IndexOutRangeException();
+		return (this->_array[i]);
+	}
+
+	unsigned int		size(void) const
+	{
+		return (this->_size);
+	}
+
+	class IndexOutRangeException : public std::exception
+	{
+		virtual const char* what() const throw()
+		{
+      			return ("Error: index out of range");
+		}
+	};
 };
 
 #endif
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/09 18:13:49 by ltouret           #+#    #+#             */
-/*   Updated: 2021/08/15 11:06:51 by ltouret          ###   ########.fr       */
+/*   Updated: 2021/08/15 20:00:01 by ltouret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,57 @@
 
 int		main(void)
 {
+	Array<int>	emptyArray;
+	Array<char>	array(10);
+	Array<char>	cpyArray(10);
+
+	// Fill array
+	for (unsigned int i = 0; i < array.size(); i++)
+		array[i] = ('0' + i);
+
+	// Show array
+	for (unsigned int i = 0; i < array.size(); i++)
+		std::cout << array[i];
+	std::cout << std::endl;
+
+	// Copy array to cpyArray
+	cpyArray = array;
+
+	// Change array
+	for (unsigned int i = 0; i < array.size(); i++)
+		array[i] = ('a' + i);
+
+	// Show cpyArray
+	for (unsigned int i = 0; i < cpyArray.size(); i++)
+		std::cout << cpyArray[i];
+	std::cout << std::endl;
+
+	// Show array
+	for (unsigned int i = 0; i < array.size(); i++)
+		std::cout << array[i];
+	std::cout << std::endl;
+
+	// Show cpyArray
+	for (unsigned int i = 0; i < cpyArray.size(); i++)
+		std::cout << cpyArray[i];
+	std::cout << std::endl;
+
+	// try exceptions 
+	try
+	{
+		std::cout << emptyArray[-1];
+	}
+	catch(const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	try
+	{
+		std::cout << array[42];
+	}
+	catch(const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 	return (0);
 }
