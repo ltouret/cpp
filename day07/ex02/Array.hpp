6151148af92bbaee286e76d33e5c016c8943eac6
LT <jiloh55@gmail.com>
2021-08-15 11:20:43 +0200

    some ex02 d07 done

---
+++ day07/ex02/Array.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltouret <ltouret@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/20 22:26:07 by ltouret           #+#    #+#             */
+/*   Updated: 2021/08/15 11:12:07 by ltouret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+
+template <typename T>
+class Array {
+private:
+    T		*Data;
+    size_t	size;
+public:
+    Array(void);
+    Array(unsigned int s);
+    //void print();
+};
+
+#endif
